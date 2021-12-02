d746e3e114c802a78ce51a780d65a7592b67ff71
Lucas Pinto <lucks@outlook.pt>
2021-12-02 13:06:57 +0000

    alterações
    Co-authored-by: PedrosGuitarLegend <PedrosGuitarLegend@users.noreply.github.com>
    Co-authored-by: Gonz4lez <Gonz4lez@users.noreply.github.com>

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,47 +1,61 @@
 <template>
-  <div class="grid">
-    <vs-row
-      style="height:720px;"
-      justify="center"
-      align="center">
-      <vs-col w="4">
-        <vs-alert
-          color="rbg(255,255,255)"
-          style="height:180px;">
-          <template #title>
-            penis seo ola
-          </template>
+  <div
+    style="max-height:720px;height:720px;overflow-y:auto;"
+    class="mr-1">
+    <div class="grid p-3">
+      <!-- style="height:200px;" -->
+      <!-- style="overflow-y:auto;height:200px" -->
+      <vs-row
+        justify="center"
+        align="center">
+        <vs-col w="6">
+          <h1>Ajuda</h1>
+          <h3>Início de Sessão</h3>
+          <p>Insira o seu nome de utilizador e clique em "Continuar"</p>
+          <img src="https://cdn.discordapp.com/attachments/890231352732057641/915721029890998302/unknown.png" alt="Início de Sessão" >
+        </vs-col>
+        <vs-col w="6">
 
-          <!-- <small class="ml-1">Nome de utilizador</small> -->
-          <vs-input
-            v-model="nome"
-            @keypress="checkSubmit"
-            @input="checkErro"
-            class="mt-3"
-            block
-            icon-before
-            label-placeholder="Nome de utilizador">
-            <template #icon>
-              <i class="fa-solid fa-user"></i>
-            </template>
-
-            <template #message-danger v-if="erro" class="mb-0">
-              {{ erro }}
-            </template>
-          </vs-input>
-
-          <vs-button
-            color="primary"
-            :disabled="!!erro"
-            @click="iniciarSessao"
-            class="ml-0">
-            Continuar
-          </vs-button>
-        </vs-alert>
-      </vs-col>
-    </vs-row>
+          <h3>Chat</h3>
+          <p>Depois de iniciar sessão, escolha uma conversa que pretende no chat lateral à esquerda</p>
+          <img src="https://cdn.discordapp.com/attachments/890231352732057641/915723573065310318/unknown.png" alt="Escolher conversa" >
+        </vs-col>
+      </vs-row>
+      <vs-row>
+        <vs-col w="6">
+          <h3>Envio de Mensagens</h3>
+          <p>Após selecionar uma conversa, no campo de texto insira a sua mensagem e carregue no enviar</p>
+          <img src="https://cdn.discordapp.com/attachments/890231352732057641/915727821018628146/unknown.png" alt="Enviar mensagem" >
+        </vs-col>
+        <vs-col w="6">
+          <h3>Pesquisa de contactos</h3>
+          <p>Pesquise ao escrever o nome no campo de texto ou filtre através do botão "filtro"</p>
+          <img src="https://cdn.discordapp.com/attachments/890231352732057641/915727821018628146/unknown.png" alt="Enviar mensagem" >
+        </vs-col>
+      </vs-row>
+      <vs-row>
+        <vs-col w="6">
+          <h3>Iniciar Chamada de Áudio</h3>
+          <p>Após selecionar uma conversa, selecione o botão "Áudio"</p>
+          <img src="https://cdn.discordapp.com/attachments/890231352732057641/915727821018628146/unknown.png" alt="Enviar mensagem" >
+        </vs-col>
+        <vs-col w="6">
+          <h3>Mudar Fundo de Vídeo</h3>
+          <p>Carregar no botão "Fundo de Vídeo, escolher o fundo e clicar em "Confirmar"</p>
+          <p>Para remover o fundo aplicado, Carregar no botão "Fundo de Vídeo" e clicar "Repor"</p>
+          <img src="https://cdn.discordapp.com/attachments/890231352732057641/915727821018628146/unknown.png" alt="Enviar mensagem" >
+        </vs-col>
+      </vs-row>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+img {
+  border: 3px solid white;
+  width: 400px;
+}
+</style>
 
 <script lang="ts">
 import Vue from 'vue';
